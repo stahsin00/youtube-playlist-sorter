a3918f15ca848e4cd6549bde90da83edfbb0dd82
stahsin00 <lamisa1998@hotmail.com>
2024-04-17 23:36:03 -0700

    add videos to playlist

--- youtube_playlist_sorter.py
+++ youtube_playlist_sorter.py
@@ -13,6 +13,7 @@ load_dotenv()
 CLIENT_SECRETS_FILE = os.getenv('CLIENT_SECRETS_PATH')
 SCOPES = [os.getenv('YOUTUBE_API_SCOPES')]
 PLAYLIST_ID = os.getenv('PLAYLIST_ID')
+TEST_PLAYLIST_ID = os.getenv('TEST_PLAYLIST_ID')
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
@@ -32,9 +33,26 @@ def get_openai_response(prompt):
 
 def get_authenticated_service():
     flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
-    credentials = flow.run_local_server(port=8080)
+    credentials = flow.run_local_server(port=8080, prompt='consent')
 
     return build('youtube', 'v3', credentials=credentials)
+
+
+def add_video_to_playlist(youtube, playlist_id, video_id):
+    request_body = {
+        'snippet': {
+            'playlistId': playlist_id,
+            'resourceId': {
+                'kind': 'youtube#video',
+                'videoId': video_id
+            }
+        }
+    }
+
+    try:
+        youtube.playlistItems().insert(part="snippet", body=request_body).execute()
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 def print_playlist_videos(youtube, playlist_id):
@@ -52,10 +70,32 @@ def print_playlist_videos(youtube, playlist_id):
         print(item['snippet']['title'])
 
 
+def get_and_add_video(youtube, playlist_id):
+    max_results=5
+    
+    try:
+        request = youtube.playlistItems().list(
+            part="snippet",
+            playlistId=playlist_id,
+            maxResults=max_results
+        )
+        response = request.execute()
+
+        items = response.get('items', [])
+        for item in items:
+            video_id = item['snippet']['resourceId']['videoId']
+            print(item['snippet']['title'])
+            add_video_to_playlist(youtube, TEST_PLAYLIST_ID, video_id)
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
 def main():
-    #youtube = get_authenticated_service()
+    youtube = get_authenticated_service()
     #print_playlist_videos(youtube,PLAYLIST_ID)
-    response = get_openai_response("hello! im just testing api calls")
-    print(response)
+    #response = get_openai_response("hello! im just testing api calls")
+    #print(response)
+    get_and_add_video(youtube, PLAYLIST_ID)
 
 main()
